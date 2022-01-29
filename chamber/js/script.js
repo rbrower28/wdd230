a2329f0116587dec19cabac3d4f3a49215da3dad
Ryan Brower <ryan.brower28@gmail.com>
2022-01-29 14:11:35 -0700

    homepage draft 1

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,7 +1,19 @@
+// current date in header
+
+const now = new Date()
+const fullDate = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+document.getElementById("date").innerHTML = fullDate;
+document.getElementById("date2").innerHTML = fullDate;
+
+// hamburger menu
 
 function openMenu() {
     document.getElementById("lower").classList.remove("hidden");
     document.getElementById("lower-nav").classList.remove("hidden");
+    document.getElementById("date2").classList.remove("hidden");
     document.getElementById("stack-bottom").classList.remove("hidden");
     document.getElementById("stack-top").classList.add("hidden");
 };
@@ -9,6 +21,7 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("lower").classList.add("hidden");
     document.getElementById("lower-nav").classList.add("hidden");
+    document.getElementById("date2").classList.add("hidden");
     document.getElementById("stack-bottom").classList.add("hidden");
     document.getElementById("stack-top").classList.remove("hidden");
 };
@@ -18,3 +31,10 @@ x.onclick = openMenu;
 
 const y = document.getElementById("stack-bottom");
 y.onclick = closeMenu;
+
+// year and last modified displayed in footer
+
+let currentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = currentYear;
+
+document.querySelector("#last-modified").textContent = document.lastModified;
